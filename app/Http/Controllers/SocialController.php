376c672c16f0d9db2a1180be892f32253d6483f9
iamgadfly <mironov@sveak.com>
2024-04-17 13:59:37 +0300

    v1.2

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -4,40 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialController extends Controller
 {
+
     public function redirectToSteam()
     {
         return Socialite::driver('steam')->redirect();
-
-//        return Socialite::driver('steam')->redirect();
+        //        return Socialite::driver('steam')->redirect();
     }
 
     //Steam callback
     public function handleSteamCallback()
     {
         $user = Socialite::driver('steam')->user();
-        dd($user);
 
-//
-//        $u = User::firstOrCreate([
-//            'email' => $data->email,
-//        ], [
-//            'name'               => $data->name,
-//            'password'           => bcrypt(Str::random(8)),
-//            'is_register_social' => true,
-//        ]);
-//
-//        $u->update(['name' => $data->name]);
-//
-//        return $this->authService->createNewToken(
-//            Auth::login($u)
-//        );
-//
-//        return $this->socialService->registerOrLoginUser(
-//            Socialite::driver('google')->user()
-//        );
+        $u = User::firstOrCreate([
+            'steam_id' => $user->id,
+        ], [
+            'name'     => $user->getName(),
+            'nickname' => $user->getNickname(),
+            'email'    => $user->getEmail(),
+            'avatar'   => $user->getAvatar(),
+            'steam_id' => $user->getId(),
+        ]);
+
+        return $this->createNewToken(
+            Auth::login($u)
+        );
     }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string  $token
+     */
+    public function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth()->factory()->getTTL() * 60,
+            'user'         => auth()->user(),
+        ]);
+    }
+
+
 }
--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,6 +1,10 @@
 @extends('head')
 
+@php
+    $user = auth()->user();
+    $__data['user'] = $user;
+@endphp
 
-@yield('content', View::make($__data[0], ['data' => $__data]))
+@yield('content', View::make($__data[0], ['data' => $__data, 'user' => 123]))
 
 @extends('footer')
